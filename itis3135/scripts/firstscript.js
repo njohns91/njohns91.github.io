e3b4dd1a6d885fcf2004739eaa60d78ffd8a5731
njohns91 <97905712+njohns91@users.noreply.github.com>
2022-04-20 00:36:35 -0400

    update

---
+++ itis3135/scripts/firstscript.js
@@ -0,0 +1,23 @@
+function scriptTest() {
+    alert("Sweet I did this right!");
+}
+function hello() {
+    let name = document.getElementById("name").value;
+    let emotion = document.getElementById("emotion").value;
+    document.getElementById("hello").innerHTML = ("<p>The Navy Jaguars welecomes you, " + name + "! You are feeling " + emotion + " today!")
+}
+function question() {
+    document.getElementById("question").innerHTML = ("Ehhh maybe")
+}
+function joke2() {
+    document.getElementById("joke2").innerHTML = ("It was two tired")
+}
+function joke3() {
+    document.getElementById("joke3").innerHTML = ("He got a hole in one")
+}
+function joke4() {
+    document.getElementById("joke4").innerHTML = ("He was stuck in a vicious cycle.")
+}
+function fact() {
+    document.getElementById("fact").innerHTML = ("A group of Tigers are called an ambush or streak.")
+}
